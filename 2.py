8c0d8a6acd7751d5c572e9b3053f4cd13ad7a1dd
Tibin Thomas <Tibin Thomas>
2016-05-28 12:45:03 +0530

    Remove verification and array print

--- 2.py
+++ 2.py
@@ -5,12 +5,9 @@ import os
 randomByteArray = bytearray(os.urandom(120000))
 
 flatNumpyArray = np.array(randomByteArray)
-print(flatNumpyArray)
 
 grayImage = flatNumpyArray.reshape(300,400)
 cv2.imwrite('RandomGray.png', grayImage)
-print(grayImage)
 
 bgrImage = flatNumpyArray.reshape(100,400,3)
 cv2.imwrite('RandomColor.png', bgrImage)
-print(bgrImage)
